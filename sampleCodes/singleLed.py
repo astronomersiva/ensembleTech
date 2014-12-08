26e7094c285679e8d367835b0934c0de1cb6153c
Sivasubramanyam A <sivasubramanyama@gmail.com>
2014-12-08 16:33:14 +0530

    organised the guide

---
+++ sampleCodes/singleLed.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+import time
+
+#turns on an LED
+#waits for two seconds
+#and switches it off
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(7, GPIO.OUT)
+GPIO.output(7, True)
+time.sleep(2)
+GPIO.output(7, False)
