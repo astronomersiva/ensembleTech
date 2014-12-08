ac05dde2e65198615b149ed20eb77f5609be9167
Sivasubramanyam A <sivasubramanyama@gmail.com>
2014-12-08 15:56:36 +0530

    added three sample codes

---
+++ twoLedConsecutive.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+#turns on an LED
+#waits for two seconds
+#and switches it off
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(7, GPIO.OUT)
+GPIO.setup(3, GPIO.OUT)
+GPIO.output(3, True)
+GPIO.output(7, True)
+time.sleep(2)
+GPIO.output(7, False)
+GPIO.output(3, False)
+
