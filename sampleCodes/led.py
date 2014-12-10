e22e498dbc7da1591f9b40e9685e33f329bc0ab4
Sivasubramanyam A <sivasubramanyama@gmail.com>
2014-12-10 13:25:13 +0530

    corrected multi led server

--- sampleCodes/led.py
+++ sampleCodes/led.py
@@ -1,28 +1,29 @@
 #!/usr/bin/pythonRoot
+
 import RPi.GPIO as GPIO     
 from flup.server.fcgi import WSGIServer 
 import sys
 import urlparse
 
 # set up our GPIO pins
-GPIO.setmode(G.BOARD)
-GPIO.setup(7, GPIO.OUT) #use any GPIO pin
-GPIO.setup(3, GPIO.OUT) #use any GPIO pin
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT) #use any GPIO pin
+GPIO.setup(2, GPIO.OUT)
 
 
 def app(environ, start_response): 
   start_response("200 OK", [("Content-Type", "text/html")])
   i = urlparse.parse_qs(environ["QUERY_STRING"])
   yield ('&nbsp;')
-  if "led" in i:
-    if i["led"][0] == "led1On": 
-      GPIO.output(7, True)   # Turn it on
-    elif i["led"][0] == "led1Off":
-      GPIO.output(7, False)  # Turn it off
-	elif i["led"][0] == "led2On":
-      GPIO.output(3, True)  # Turn it off
-	elif i["led"][0] == "led2Offf":
-      GPIO.output(3, False)  # Turn it off
-
+  if "status" in i:
+	if i["status"][0] == "1on":
+		GPIO.output(4,True)
+	if i["status"][0] == "1off":
+		GPIO.output(4,False)
+	if i["status"][0] == "2on":
+		GPIO.output(2,True)
+	if i["status"][0] == "2off":
+		GPIO.output(2,False)
+		
 
 WSGIServer(app).run()
